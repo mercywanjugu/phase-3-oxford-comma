95017599056f2fffa8194ccf81e471e02bd46df0
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-08-20 15:01:14 +0300

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.length==1
+        array[0]
+    else
+        array.slice(0,array.length-1)
+        .join(", ")+(array.length==2?" and ":", and ")+array[-1]
+    end
 end
+puts oxford_comma(["me","you","us"])
